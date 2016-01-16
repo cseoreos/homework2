3d5f0f2eda3a865d818110024dae3465115b066e
achava <achava@ucsd.edu>
2016-01-15 23:39:51 -0800

    Works! Now we have to optimize it so that we can make it work for n cases and also later have to make it take a file as input, but that should not take long at all.

--- DFS.py
+++ DFS.py
@@ -4,53 +4,41 @@ def depth_first_search():
 	while frontier:
 		node = frontier.pop()
 		if (check_if_goal(node)):
-			print "found"
-			finalnode.append(node)
-			return True
-		if node in visited: continue
-		visited.append(node)
+			return node
+		if [node[0], node[1], node[2]] in visited: continue
+		visited.append([node[0], node[1], node[2]])
 		childbranch = getchildren(node)
-		#print childbranch
 		for child in childbranch:
-			if child in visited: continue
+			if [child[0], child[1], child[2]] in visited: continue
 			frontier.append(child)
 			print child
-			dictionary[child] = node
-			print dictionary
 			
 def getchildren(node):
 	children = []
 	#sucking
 	if(node[2] == 0 and node[0] != 0):
-		children.append((0, node[1], node[2]))
+		children.append((0, node[1], node[2], node[3]+"S"))
 	elif(node[2] == 1 and node[1] != 0):
-		children.append((node[0], 0, node[2]))
-
-	#Left
-	if(node[2] ==1):
-		children.append((node[0], node[1], 0))
+		children.append((node[0], 0, node[2], node[3]+"S"))
 
 	#Right
 	if(node[2] == 0):
-		children.append((node[0], node[1], 1))
+		children.append((node[0], node[1], 1, node[3]+"R"))
+
+	#Left
+	if(node[2] ==1):
+		children.append((node[0], node[1], 0, node[3]+"L"))
+
 	return children
 
 vac = raw_input("Enter: ")
 nums = vac.split(", ")
 frontier = []
-finalnode = []
 visited = []
-dictionary = {}
-frontier.append((int(nums[0]), int(nums[1]), int(nums[2])) )
-initial = frontier[0]
-if(check_if_goal(frontier[0])):
+frontier.append((int(nums[0]), int(nums[1]), int(nums[2]), ""))
+initial = (int(nums[0]), int(nums[1]), int(nums[2]), "")
+if(check_if_goal(initial)):
 	print ""
 	quit()
-if (depth_first_search() == True):
-	#print finalnode[0]
-	while(finalnode[0] != None):
-		print finalnode[0]
-		if (finalnode[0] != initial):
-			finalnode[0] = dictionary[finalnode[0]] 
-		else:
-			finalnode[0] = None
+final = depth_first_search()
+print final[3]
